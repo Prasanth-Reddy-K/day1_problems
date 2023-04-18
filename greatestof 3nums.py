0544673b538a6fca6cd553be4eceaa704fc653c2
Prasanth-Reddy-K <karnatakamprasanthreddy@gmail.com>
2023-04-18 12:52:22 +0530

    greatest of 3nums

---
+++ greatestof 3nums.py
@@ -0,0 +1,9 @@
+a=int(input("Enter a value :"))
+b=int(input("Enter a value :"))
+c=int(input("Enter a value :"))
+if(a>=b and a>=c):
+    print("Greatest number is": a)
+elif(a<=b and c<=b):
+    print("Greatest number is": b)
+else:
+    print("Greatest number is": c)
